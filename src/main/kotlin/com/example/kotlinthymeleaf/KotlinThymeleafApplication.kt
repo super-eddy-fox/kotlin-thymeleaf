2b5a2b8349f5309d15075c84121e7a502d26cc72
super eddy <90597905+super-eddy-fox@users.noreply.github.com>
2021-11-01 16:33:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotlinthymeleaf/KotlinThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinthymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinThymeleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinThymeleafApplication>(*args)
+}
